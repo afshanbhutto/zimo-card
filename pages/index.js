04220a46c024bb7c702fe23c757d6dd86d994e89
afshanbhutto <afshanbhutto6@gmail.com>
2023-06-19 15:14:12 +0500

    added spacing

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,10 @@ export default function Home() {
     <div className="container w-full mx-auto">
       <div className="flex flex-col">
       <div className="upperDiv flex justify-between items-center mb-2 w-full uppercase min-w-[350px] px-2 md:px-0">
-      <div className="flex-1 text-left text-xl  md:text-3xl" >Featured</div>
-      <div className="flex-1 text-center text-3xl hidden md:block">View all Homes</div>
-      <div className="flex-1 text-right text-black text-5xl md:text-6xl">
-        <span >&lt;</span>
+      <div className="flex-1 text-left text-2xl tracking-[2px]  md:text-3xl" >Featured</div>
+      <div className="flex-1 text-center text-3xl tracking-[2px] hidden md:block">View all Homes</div>
+      <div className="flex-1  text-right text-black text-5xl md:text-6xl">
+        <span className="mr-4">&lt;</span>
         <span>&gt;</span>
       </div>
       </div>
